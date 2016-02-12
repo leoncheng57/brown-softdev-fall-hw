c95409bd20bf2de6c187eb88689e816cd9feb304
TESLA <57leonardo@gmail.com>
2016-02-11 23:21:31 -0500

    fixed x and y coors prob

--- 02/dotconn.js
+++ 02/dotconn.js
@@ -31,25 +31,35 @@ var drawLine = function drawLine(ctx, x1, y1, x2, y2){
 
 drawLine(ctx, 40, 40, 200, 200);
 
+/* DRAW A CIRCLE/DOT (ctx, x, y) */
+var drawCircle = function drawCircle(ctx, x, y){
+  ctx.beginPath();
+  ctx.strokeStyle="#cf2424";
+  ctx.fillStyle = "#cf2424";
+  ctx.arc(x, y, 15, 0, 2*Math.PI);
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
+  console.log("circle is drawn");
+  console.log("incircle. x: "+x + ", " + "y: "+ y);
+};
+
+drawCircle(ctx, 400, 400);
+
+
 /* FIND WHAT THE COORS OF THE MOUSE ARE WHEN CLICKED*/
 var mouseCoors = function mouseCoors(event){
   var x = event.pageX;
   var y = event.pageY;
+  //To fix the x and y coordinates to be relative to the canvas element box
+  //(0,0) is at the top left corner of the canvas box
+  x -= c.offsetLeft;
+  y -= c.offsetTop;
   console.log("x: "+x + ", " + "y: "+ y);
+  drawCircle(ctx, x, y);
 };
 
 c.addEventListener("click", mouseCoors);
-
-/* DRAW A CIRCLE/DOT (ctx, x, y) */
-var drawCircle = function drawCircle(ctx, x, y){
-  ctx.beginPath();
-  ctx.fillStyle = "#cf2424";
-  ctx.arc(x, y, 15, 0, 2*Math.PI);
-  ctx.fill();
-  ctx.closePath();
-};
-
-drawCircle(ctx, 400, 400);
 
 
 
