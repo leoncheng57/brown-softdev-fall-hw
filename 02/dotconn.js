42fc3dc5c9edf2eb1caffc71a85352c55da5f7e4
TESLA <57leonardo@gmail.com>
2016-02-11 23:06:10 -0500

    drawline and drawcircle

--- 02/dotconn.js
+++ 02/dotconn.js
@@ -10,6 +10,8 @@ HW2 -- Dot, Dot, Dot
 Use preventDefault
   function(e)
   e.preventDefault()
+.clearRect(a, b, c, d)
+<id>.addEventListener("click", fxn)
 */
 
 console.log("hello")
@@ -17,7 +19,8 @@ console.log("hello")
 var c = document.getElementById("playground")
 var ctx = c.getContext("2d")
 
-var drawLine = function drawLine(x1, y1, x2, y2){
+/* DRAW A LINE (ctx, x1, y1, x2, y2)*/
+var drawLine = function drawLine(ctx, x1, y1, x2, y2){
   ctx.beginPath();
   ctx.strokeStyle="#FF0000";
   ctx.moveTo(x1, y1);
@@ -26,9 +29,27 @@ var drawLine = function drawLine(x1, y1, x2, y2){
   ctx.closePath();
 };
 
-drawLine(40, 40, 200, 200);
+drawLine(ctx, 40, 40, 200, 200);
 
-//Use this => .clearRect(a, b, c, d)
+/* FIND WHAT THE COORS OF THE MOUSE ARE WHEN CLICKED*/
+var mouseCoors = function mouseCoors(event){
+  var x = event.pageX;
+  var y = event.pageY;
+  console.log("x: "+x + ", " + "y: "+ y);
+};
+
+c.addEventListener("click", mouseCoors);
+
+/* DRAW A CIRCLE/DOT (ctx, x, y) */
+var drawCircle = function drawCircle(ctx, x, y){
+  ctx.beginPath();
+  ctx.fillStyle = "#cf2424";
+  ctx.arc(x, y, 15, 0, 2*Math.PI);
+  ctx.fill();
+  ctx.closePath();
+};
+
+drawCircle(ctx, 400, 400);
 
 
 
