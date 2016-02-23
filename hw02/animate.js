2df60d508e1cb7f708142e43f7cfdc6f497c02ec
TESLA <57leonardo@gmail.com>
2016-02-22 20:58:13 -0500

    in progress

--- hw02/animate.js
+++ hw02/animate.js
@@ -3,19 +3,36 @@
 // HW02 -- Circular
 // 2016-02-22
 
-if (growing) (radius = radius+1);
-else (radius = radius-1);
 
-if (radius == c.width/2)
-   growing = false;
-else if (radius == 0){
-     growing = true;
+console.log("beginning js file")
+
+
+//Get Canvas Element
+var c = document.getElementById("pad");
+//Get 2d Context
+var ctx = c.getContext("2d");
+
+var growing = true;
+var radius = c.height/2;
+
+if (growing){
+  radius += 1;
 }
-ctx.beginPath();
-ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
-ctx.stroke();
-ctx.fill();
+else{
+  radius -= 1;
+}
 
-window.requestAnimationFrame( drawDot );
-
-};
+// if (radius == c.width/2)
+//    growing = false;
+// else if (radius == 0){
+//      growing = true;
+// }
+// ctx.beginPath();
+// ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
+// ctx.stroke();
+// ctx.fill();
+//
+// window.requestAnimationFrame( drawDot );
+//
+// };
+//TODO: ADD COMMENTS
