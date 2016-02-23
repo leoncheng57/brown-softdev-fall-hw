3360fd3761ed3a27879760c4c6296876df6a8295
TESLA <57leonardo@gmail.com>
2016-02-23 10:06:53 -0500

    renamed resetting to stop

--- hw03/animate.js
+++ hw03/animate.js
@@ -12,7 +12,6 @@ var ctx = c.getContext("2d");
 
 var growing = true; //boolean to keep track of growing or shrinking
 var radius = 0;
-// var is_resetting = false;
 var requestId; //ID for the Animation Frame to stop or to keep going
 
 
@@ -44,30 +43,20 @@ var drawDot = function(){
   ctx.fill();
   ctx.closePath();
 
-  //Exit the loop of callbacks if reset button is pressed
-  // if(is_resetting){
-    // return;
-  // }
-  // else{
-    // Periodically Call the Function
-    requestId = window.requestAnimationFrame(drawDot);
-  // }
+  // Periodically Call the Function
+  requestId = window.requestAnimationFrame(drawDot);
 }
 
-//If start is called, stop the reset action, strat calling the drawDot callbacks loop
+//If start is called, start calling the drawDot callbacks loop
 var sb = document.getElementById("start");
 sb.addEventListener("click", function(){
-  // is_resetting = false;
   drawDot();
 });
 
-//If reset is pressed, set the variable true so that the drawDot callbacks loop is stopped, also set radius = 0
-var rb = document.getElementById("reset");
+//If stop is pressed, set the variable true so that the drawDot callbacks loop is stopped
+var rb = document.getElementById("stop");
 rb.addEventListener("click", function(){
-  // is_resetting = true;
   stopIt();
-  // radius = 0;
-  // growing = true;
 });
 
 
