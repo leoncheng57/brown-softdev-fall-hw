a15cbda16ce712ad6800b7ba2cf68f36e8580e1a
TESLA <57leonardo@gmail.com>
2016-02-23 22:55:21 -0500

    done adding dvd logo

--- hw03/animate.js
+++ hw03/animate.js
@@ -24,59 +24,109 @@ var requestId;
 
 var drawDot = function(){
 
-  // Change Radius by increasing or decreasing
-  if (growing){
-    radius += 1;
-  }
-  else{
-    radius -= 1;
-  }
+    // Change Radius by increasing or decreasing
+    if (growing){
+	radius += 1;
+    }
+    else{
+	radius -= 1;
+    }
 
-  // Set it to either Growing or Shrinking
-  if (radius == c.width/2){
-    growing = false;
-  }
-  else if (radius == 0){
-    growing = true;
-  }
+    // Set it to either Growing or Shrinking
+    if (radius == c.width/2){
+	growing = false;
+    }
+    else if (radius == 0){
+	growing = true;
+    }
 
-  //Draw the circle
-  ctx.clearRect(0, 0, c.width, c.height);
-  ctx.beginPath();
-  ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
-  ctx.fillStyle = "#ff6c24" //orange
-  ctx.stroke();
-  ctx.fill();
-  ctx.closePath();
+    //Draw the circle
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+    ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
+    ctx.fillStyle = "#ff6c24" //orange
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
-  // Periodically Call the Function
-  requestId = window.requestAnimationFrame(drawDot);
+    // Periodically Call the Function
+    requestId = window.requestAnimationFrame(drawDot);
 }
 
 //If start is pressed, start calling the drawDot callbacks loop
 var sb = document.getElementById("start");
 sb.addEventListener("click", function(){
-  drawDot();
+    drawDot();
 });
 
 //If stop is pressed, set the variable true so that the drawDot callbacks loop is stopped
 var rb = document.getElementById("stop");
 rb.addEventListener("click", function(){
-  stopIt();
+    stopIt();
 });
 
 //Function to stop the animation
 var stopIt = function(){
-  window.cancelAnimationFrame(requestId)
+    window.cancelAnimationFrame(requestId)
 }
 
 
 /*===CODE FOR LOGO===*/
 
+//Arbitrarily pick two numbers for heigh and width
+var lheight = 100;
+var lwidth = 100;
+
+//Loading Image
 var logo = new Image();
 logo.src = "logo_dvd.jpg";
-ctx.fillStyle = "#ff6c24";
-ctx.drawImage(logo, c.width/2, c.height/2, 100, 100);
 
+//Booleans to keep track of direction of movement
+var goingUp = false;
+var goingRight = true;
+
+//X and Y Coors of the Logo
+var lx = Math.random()*(c.width-lwidth)+1;
+var ly = Math.random()*(c.height-lheight)+1;
+
+var drawLogo = function(){
+
+    //Clear drawing pad first
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    //Move in vertical direction
+    if (goingUp)
+	ly += 1;
+    else
+	ly -= 1;
+
+    //Move in horizontal directoin
+    if (goingRight)
+	lx += 1;
+    else
+	lx -= 1;
+
+    //Change the direction when it hits the wall
+    if (lx <= 0 || lx >= c.width-lwidth)
+	goingRight = !goingRight;
+    if(ly <= 0 || ly >= c.height-lheight)
+	goingUp = !goingUp;
+
+    //Draw the image
+    ctx.drawImage(logo, lx, ly, lwidth, lheight);
+
+    //Periodically call the function
+    window.requestAnimationFrame(drawLogo);
+}
+
+//When logo button is pressed, call drawLogo
+var lb = document.getElementById("logo");
+lb.addEventListener("click", function(){
+    //Randomize lx and ly
+    lx = Math.random()*(c.width-lwidth)+1;
+    ly = Math.random()*(c.height-lheight)+1;
+
+    drawLogo();
+});
 
 console.log("end of js file")
