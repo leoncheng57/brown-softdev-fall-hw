83e920d0f19fa7d05541b6d3d8c515c0c79f08f2
TESLA <57leonardo@gmail.com>
2016-02-25 01:32:19 -0500

    better comments

--- hw03/animate.js
+++ hw03/animate.js
@@ -10,8 +10,6 @@ var c = document.getElementById("pad");
 //Get 2d Context
 var ctx = c.getContext("2d");
 
-
-
 /*===CODE FOR CIRCLE===*/
 
 //Boolean to keep track of growing or shrinking
@@ -21,9 +19,7 @@ var radius = 0;
 // ID for the Animation Frame to stop or to keep going
 var requestId;
 
-
 var drawDot = function(){
-
     // Change Radius by increasing or decreasing
     if (growing){
 	radius += 1;
@@ -31,7 +27,6 @@ var drawDot = function(){
     else{
 	radius -= 1;
     }
-
     // Set it to either Growing or Shrinking
     if (radius == c.width/2){
 	growing = false;
@@ -39,7 +34,6 @@ var drawDot = function(){
     else if (radius == 0){
 	growing = true;
     }
-
     //Draw the circle
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.beginPath();
@@ -48,21 +42,14 @@ var drawDot = function(){
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
-
     // Periodically Call the Function
     requestId = window.requestAnimationFrame(drawDot);
 }
 
-//If start is pressed, start calling the drawDot callbacks loop
+//Button: If start is pressed, start calling the drawDot callbacks loop
 var sb = document.getElementById("start");
 sb.addEventListener("click", function(){
     drawDot();
-});
-
-//If stop is pressed, set the variable true so that the drawDot callbacks loop is stopped
-var rb = document.getElementById("stop");
-rb.addEventListener("click", function(){
-    stopIt();
 });
 
 //Function to stop the animation
@@ -70,56 +57,76 @@ var stopIt = function(){
     window.cancelAnimationFrame(requestId)
 }
 
+//Button: If stop is pressed, set the variable true so that the drawDot callbacks loop is stopped
+var rb = document.getElementById("stop");
+rb.addEventListener("click", function(){
+    stopIt();
+});
+
 
 /*===CODE FOR LOGO===*/
 
 //Arbitrarily pick two numbers for heigh and width
 var lheight = 100;
 var lwidth = 100;
-
 //Loading Image
 var logo = new Image();
 logo.src = "logo_dvd.jpg";
-
 //Booleans to keep track of direction of movement
 var goingUp = false;
 var goingRight = true;
-
 //X and Y Coors of the Logo
 var lx = Math.random()*(c.width-lwidth)+1;
 var ly = Math.random()*(c.height-lheight)+1;
 
 var drawLogo = function(){
-
     //Clear drawing pad first
     ctx.clearRect(0, 0, c.width, c.height);
-
     //Move in vertical direction
     if (goingUp)
 	ly += 1;
     else
 	ly -= 1;
-
     //Move in horizontal directoin
     if (goingRight)
 	lx += 1;
     else
 	lx -= 1;
-
     //Change the direction when it hits the wall
     if (lx <= 0 || lx >= c.width-lwidth)
 	goingRight = !goingRight;
     if(ly <= 0 || ly >= c.height-lheight)
 	goingUp = !goingUp;
-
     //Draw the image
     ctx.drawImage(logo, lx, ly, lwidth, lheight);
-
     //Periodically call the function
     window.requestAnimationFrame(drawLogo);
 }
 
-//When logo button is pressed, call drawLogo
+var dvdLogoSetup = function() {
+    
+    //Q: What good might this do?
+    //window.cancelAnimationFrame( requestID );
+   
+    //var inits
+    //...
+
+
+    //a function defined within a function, oh my!
+    var dvdLogo = function() {
+	
+	//propulsion mechanism
+	//...
+
+	//Q: Why this here?
+	requestID = window.requestAnimationFrame( dvdLogo );		
+    };
+
+    dvdLogo();
+};
+
+
+//Button: When logo button is pressed, call drawLogo
 var lb = document.getElementById("logo");
 lb.addEventListener("click", function(){
     //Randomize lx and ly
