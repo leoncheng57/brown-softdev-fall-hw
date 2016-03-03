3d02409f517515bb39bb661c6581fa1fe84ef1fd
TESLA <57leonardo@gmail.com>
2016-03-03 17:16:07 -0500

    circle done

--- hw04/svg-interactive.js
+++ hw04/svg-interactive.js
@@ -1,9 +1,16 @@
 console.log("beginning js file")
 
-//testing window.setInterval
-window.setInterval(function(){
-  console.log("tick");
-}, 16);
+var boxLen= 500;
+
+// //testing window.setInterval
+// intervalID = window.setInterval(function(){
+//     console.log("tick");
+// }, 16);
+
+// var stop = function(){
+//     window.clearInterval(intervalID);
+// };
+
 
 //Get SVG element
 var pic = document.getElementById("vimage");
@@ -16,31 +23,40 @@ var radius = 0;
 var intervalID;
 
 var drawDot = function(){
-  //Stops the circle from accelerating in size if you spammed the start button, by cancelling the previous call
-  window.clearInterval(intervalID);
-  // Change Radius by increasing or decreasing
-  if(growing)
-    radius += 1;
-  else
-    radius += 1;
-  // Set it to either Growing or Shrinking
-  if(radius==pic.width/2)
-    growing=false;
-  else if (radius==0)
-    growing = true;
-  //Draw the circle
-  var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-  c.setAttribute("cx", pic.width/2);
-  c.setAttribute("cy", pic.height/2);
-  c.setAttribute("r", radius);
-  c.setAttribute("fill", "orange");
-  c.setAttribute("stroke", "black");
-  pic.appendChild(c);
-  // Periodically Call the Function
-  // requestId = window.requestAnimationFrame(drawDot);
+    pic.removeChild(pic.childNodes[0]);
+    // Change Radius by increasing or decreasing
+    if(growing)
+	radius += 1;
+    else
+	radius -= 1;
+    // Set it to either Growing or Shrinking
+    if(radius==boxLen/2)
+	growing=false;
+    else if (radius==0)
+	growing = true;
+    //Draw the circle
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    c.setAttribute("cx", boxLen/2);
+    c.setAttribute("cy", boxLen/2);
+    c.setAttribute("r", radius);
+    c.setAttribute("fill", "orange");
+    c.setAttribute("stroke", "black");
+    pic.appendChild(c);
+    // Periodically Call the Function
+    // requestId = window.requestAnimationFrame(drawDot);
 }
 
-window.setInterval(drawDot, 16);
+var start = document.getElementById("start");
+start.addEventListener("click", function(){
+    window.clearInterval(intervalID);
+    intervalID = window.setInterval(drawDot, 16)
+});
+
+var stop = document.getElementById("stop");
+stop.addEventListener("click", function(){
+    //Stops the circle from accelerating in size if you spammed the start button, by cancelling the previous call
+    window.clearInterval(intervalID);
+});
 
 
 console.log("ending js file")
