e66d1437ab78e4b9f48cb9cc3cc51e194f9e8871
TESLA <57leonardo@gmail.com>
2016-03-19 20:22:54 -0400

    dictionary->bars and states works git add -A :)

--- hw05/index.js
+++ hw05/index.js
@@ -1,9 +1,34 @@
-var data = [4,8,15,16,23,42];
+// var delegates = [58, 18, 10, 75, 37];
+
+
+// d3.select(".chart")
+//     .selectAll("div")
+//       .data(delegates)
+//     .enter().append("div")
+//       .style("width", function(d) {
+//           return d*10 + "px"; })
+//       .text(function(d) { return d; })
+
+// d3.select("body").append("p").text("New paragarph!");
+
+//IDEA: PUT THESE IN A CSV FILE, THEN LOAD IT IN
+var better = [
+    { state: "New York", count: 900},
+    { state: "California", count: 410},
+    { state: "Texas", count: 130},
+    { state: "New Hampshire", count: 310},
+    { state: "Washington", count: 102},
+];
+
 d3.select(".chart")
     .selectAll("div")
-      .data(data)
+    .data(better)
     .enter().append("div")
-      .style("width", function(d) {
-          return d*10 + "px"; })
-      .text(function(d) { return d; });
+    .style("width", function(d){
+	return d.count + "px";})
+    .text(function(d){
+	return d.state + ": " + d.count;
+    })
 
+
+    
