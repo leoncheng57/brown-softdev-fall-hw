0b0846d1f576c2c28606d078c2f887d59e602de1
TESLA <57leonardo@gmail.com>
2016-03-31 23:48:01 -0400

    task2

--- hw06/testing.py
+++ hw06/testing.py
@@ -35,8 +35,6 @@ print [1
 # TASK 1
 # minimum threshold: at least one uppercase, lowercase, number
 
-PWORD = "Hello123"
-
 def task1(s):
     l = [1 if x in DIGITS
             else 2 if x in UC_LETTERS
@@ -45,8 +43,39 @@ def task1(s):
             for x in s]
     return (1 in l) and (2 in l) and (3 in l)
 
-print task1(PWORD)
+print task1("Hello123")
 
 # TASK 2
 # password strength rating
-# 
+# weak password - return low integer
+# options: mixture of upper and lower, nums, non-alphanumeric chars, 
+# also length
+
+def task2(s):
+    l = [1 if x in UC_LETTERS
+         else 2 if x in LC_LETTERS
+         else 3 if x in DIGITS
+         else 4 if x in ".?!&#,;:-_*"
+         else 0
+         for x in s]
+    #find how many unique chars types there are
+    result = 0
+    if 0 in l:
+        result += 1
+    if 1 in l:
+        result += 1
+    if 2 in l:
+        result += 1
+    if 3 in l:
+        result += 1
+    if 4 in l:
+        result += 1
+    result *= len(s)**3 #emphasize influence of pword length
+    result = -1*(1.0/(0.00001*(result+10000)))+10 #this math formula is used to put numbers on a 0-10 scale
+    return result
+        
+print task2("123")
+print task2("password")
+print task2("BetterPasswod!!")
+print task2("Hello123!@@!##$!@$!%")
+print task2("hellowhatishappeninghtereinlifehonesltythosnowwhatlonger")
