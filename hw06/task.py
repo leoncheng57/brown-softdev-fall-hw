f72fe6268bda1cd0c0d842a57009d18539391691
TESLA <57leonardo@gmail.com>
2016-03-31 23:52:35 -0400

    complet

--- hw06/task.py
+++ hw06/task.py
@@ -1,36 +1,6 @@
-squares = []
-for x in range(8):
-    squares.append(x**2)
-
-print squares
-
-print [x for x in range(8)]
-print [x*x for x in range(8)]
-
-print [ (x, x*x, x*x*x) for x in range(8) ]
-
-print [ (x, x*x, x**3) for x in range(8) ]
-
-p = "myNoobPass1234"
-
-print p
-
-print [x for x in p]
-print [x for x in "1234"]
-
 UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LC_LETTERS = "abcdefghijklmnopqrstuvwxyz"
 DIGITS = "0123456789"
-
-print [x for x in p if x in UC_LETTERS]
-
-print [1 for x in p if x in UC_LETTERS ]
-print [1 if x in UC_LETTERS else 0 for x in p]
-
-print [1
-       if x in UC_LETTERS
-       else 0
-       for x in p]
 
 # TASK 1
 # minimum threshold: at least one uppercase, lowercase, number
@@ -79,3 +49,4 @@ print task2("password")
 print task2("BetterPasswod!!")
 print task2("Hello123!@@!##$!@$!%")
 print task2("hellowhatishappeninghtereinlifehonesltythosnowwhatlonger")
+
