1262f11ab11938dbb27565ccd736b317af4b137b
TESLA <57leonardo@gmail.com>
2016-04-03 17:20:57 -0400

    closures assignment

--- hw07/task.py
+++ hw07/task.py
@@ -1,15 +1,23 @@
 def repeat(s):
-    def r1(n):
-        i = 0;
-        result=""
+    def r(n):
+        i = 0
+        result = ""
         while(i<n):
-            result+=s
+            result += s
             i+=1
         return result
-    return r1
+    return r
+
+def r1(n):
+    return repeat("hello")(n)
+
+def r2(n):
+    return repeat("goodbye")(n)
+
+def r3(n):
+    return repeat("closures!")(n)
 
 print repeat("cool")(3)
-
-def r1
-
-def r2
+print r1(2)
+print r2(2)
+print r3(3)
