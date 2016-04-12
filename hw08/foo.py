41f133d181f28bd0deb8215862c3a83b1e727c83
TESLA <57leonardo@gmail.com>
2016-04-12 01:38:36 -0400

    redid in my own code

--- hw08/foo.py
+++ hw08/foo.py
@@ -1,17 +1,32 @@
-def union(A, B):
-    return [x for x in A if x not in B] + B
+def union(a, b):
+    return [x for x in a if x not in b] + b
 
-def set_difference(a, b):
-    return [x for x in a if x not in b]
-
-def cartesian_product(a, b):
-    return [(x, y) for x in a for y in b]
+print union( [1, 2, 3],
+             [2, 3, 4])
 
 def inter(a, b):
     return [x for x in a if x in b]
+    
+print inter( [1, 2, 3],
+             [2, 3, 4])
 
-def sym_diff(A, B):
-    return union(set_diff(a, b), set_diff(b, a))
+def set_diff(a, b):
+    return [x for x in a if x not in b]
 
-print union([1,2,3,4,5],[5,6,7])
+print set_diff( [1, 2, 3],
+                [2, 3, 4])
 
+print set_diff( [2, 3, 4],
+                [1, 2, 3])
+
+def sym_diff(a, b):
+    return set_diff( union(a, b), inter(a, b) )
+
+print sym_diff( [1, 2, 3],
+                [2, 3, 4])
+
+def cart_prod(a, b):
+    return [(x, y) for x in a for y in b]
+
+print cart_prod( [1, 2],
+                 ["red","white"])
